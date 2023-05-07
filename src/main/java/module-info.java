0944fd591042a2ba829b92647ccc16a98ef8b13b
Ahmed <ahmedalaa555r@gmail.com>
2023-05-07 21:29:06 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.fcdslibraryv2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.fcdslibraryv2 to javafx.fxml;
+    exports com.example.fcdslibraryv2;
+}
