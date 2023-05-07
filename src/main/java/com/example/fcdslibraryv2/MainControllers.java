ee54dde76b8e585f62e3b1a8afdae10bb732dd6f
Ahmed <ahmedalaa555r@gmail.com>
2023-05-08 00:30:07 +0300

    seperated search function from initalize
    solved a bug in delete function
    
    everything is working properly 👍
    TODO NEXT: optimize search

--- src/main/java/com/example/fcdslibraryv2/MainControllers.java
+++ src/main/java/com/example/fcdslibraryv2/MainControllers.java
@@ -54,56 +54,56 @@ public class MainControllers implements Initializable {
 
     @FXML
     private TableColumn<Book, String> genre;
-    
-    
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
 
-//        Library.getInstance().addBook("123","The Great Gatsby", "F. Scott Fitzgerald", "1925", "Scribner","Fiction");
-//        Library.getInstance().addBook("456","To Kill a Mockingbird", "Harper Lee", "1960", "J. B. Lippincott & Co.","Fiction");
-    	ObservableList<Book> Books_updated = FXCollections.observableList(Library.getInstance().getlist());
-        library.setItems(Books_updated);
-      //searching functions
-        
+    ObservableList<Book> Books_updated;
+    
+    public void search(){
+        //searching functions
+
         //filtered list
-        FilteredList<Book> booksrch = new FilteredList<>(Books_updated,b ->true); 
-        
+        FilteredList<Book> booksrch = new FilteredList<>(Books_updated,b ->true);
+
         //linking  list to searchbox
-        
+
         srch.textProperty().addListener((observable, oldValue, newValue) -> {
-			booksrch.setPredicate(book -> {
-				// If filter text is empty, display all books.
-								
-				if (newValue == null || newValue.isEmpty()) {
-					return true;
-				}
-				
-				//getting the string in lowercase
-				String lowerCaseFilter = newValue.toLowerCase();
-				
-				if (book.getIsbn().startsWith(lowerCaseFilter)) {
-					return true; // Filter by ISBN.
-				} else if (book.getTitle().toLowerCase().contains(lowerCaseFilter)) {
-					return true; // Filter by book title.
-				}
-				else if (book.getAuthor().toLowerCase().contains(lowerCaseFilter)) {
-				     return true;//Filter by book author
-				     }
-				     else  {
-				    	 return false; // Does not match.
-				     }}
-				     );
-			});
+            booksrch.setPredicate(book -> {
+                // If filter text is empty, display all books.
+
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+
+                //getting the string in lowercase
+                String lowerCaseFilter = newValue.toLowerCase();
+
+                if (book.getIsbn().startsWith(lowerCaseFilter)) {
+                    return true; // Filter by ISBN.
+                } else if (book.getTitle().toLowerCase().contains(lowerCaseFilter)) {
+                    return true; // Filter by book title.
+                }
+                else if (book.getAuthor().toLowerCase().contains(lowerCaseFilter)) {
+                    return true;//Filter by book author
+                }
+                else  {
+                    return false; // Does not match.
+                }}
+            );
+        });
         //sorting the filtered list
         SortedList<Book> sortedData = new SortedList<>(booksrch);
-		//using comparator to sort the books
-		sortedData.comparatorProperty().bind(library.comparatorProperty());
-		//setting the books in the table
-		library.setItems(sortedData);
-		
-		
-		
-        // Initialize the columns
+        //using comparator to sort the books
+        sortedData.comparatorProperty().bind(library.comparatorProperty());
+        //setting the books in the table
+        library.setItems(sortedData);
+    }
+    public void load_data(){
+        Books_updated = FXCollections.observableList(Library.getInstance().getlist());
+        library.setItems(Books_updated);
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //load data
+        load_data();
         // Initialize the columns
         isbn.setCellValueFactory(new PropertyValueFactory<>("isbn"));
         title.setCellValueFactory(new PropertyValueFactory<>("title"));
@@ -111,7 +111,8 @@ public class MainControllers implements Initializable {
         release_year.setCellValueFactory(new PropertyValueFactory<>("release_year"));
         publisher.setCellValueFactory(new PropertyValueFactory<>("publisher"));
         genre.setCellValueFactory(new PropertyValueFactory<>("genre"));
-
+        //search
+        search();
         deleteBtn.disableProperty().bind(
                 Bindings.isEmpty(library.getSelectionModel().getSelectedItems())
         );
@@ -123,21 +124,14 @@ public class MainControllers implements Initializable {
         Book selectedBook = library.getSelectionModel().getSelectedItem();
         if (selectedBook != null) {
             Library.getInstance().deleteBook(selectedBook);
-            library.getItems().remove(selectedBook);
-            library.refresh();
+//            library.getItems().remove(selectedBook);
+//            Books_updated.remove(selectedBook);
+//            library.refresh();
         }
+        load_data();
     }
     @FXML
     public void refreshScene() throws IOException {
         library.refresh();
     }
-    
-    
-    
-    
-    
-    
-    
-    
-
 }
